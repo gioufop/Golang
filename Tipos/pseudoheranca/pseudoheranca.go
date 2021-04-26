4e5d625109afc54762d80a5e790a0aa87dfa67d7
Giovanni <giovanni@stilingue.com.br>
2021-04-26 15:53:54 -0300

    pseudoheranca

---
+++ Tipos/pseudoheranca/pseudoheranca.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type carro struct {
+	nome           string
+	velcidadeAtual int
+}
+
+type ferrari struct {
+	carro       // campos anonimos
+	turboLigado bool
+}
+
+func main() {
+	f := ferrari{}
+	f.nome = "F40"
+	f.velcidadeAtual = 0
+	f.turboLigado = true
+
+	fmt.Printf("A ferrari %s está com o turbo ligado? %v\n", f.nome, f.turboLigado)
+	fmt.Println(f)
+}
